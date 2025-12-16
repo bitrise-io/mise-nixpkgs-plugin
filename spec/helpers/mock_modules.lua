02423ddb6c6136c2ad241afb1e2933ddb11ee87c
Olivér Falvai <oliver.falvai@bitrise.io>
2025-12-16 11:20:05 +0100

    Fix formatting task

--- spec/helpers/mock_modules.lua
+++ spec/helpers/mock_modules.lua
@@ -1,36 +1,36 @@
 local M = {}
 
 function M.mock_file_module()
-	return {
-		read = function(path)
-			return '{"pkgs":{}}'
-		end,
-		join_path = function(...)
-			return table.concat({ ... }, "/")
-		end,
-	}
+    return {
+        read = function(path)
+            return '{"pkgs":{}}'
+        end,
+        join_path = function(...)
+            return table.concat({ ... }, "/")
+        end,
+    }
 end
 
 function M.mock_cmd_module()
-	return {
-		exec = function(cmd)
-			return "success"
-		end,
-	}
+    return {
+        exec = function(cmd)
+            return "success"
+        end,
+    }
 end
 
 function M.inject_modules(mocks)
-	for name, mock in pairs(mocks) do
-		package.loaded[name] = mock
-	end
+    for name, mock in pairs(mocks) do
+        package.loaded[name] = mock
+    end
 end
 
 function M.clear_modules()
-	package.loaded["file"] = nil
-	package.loaded["json"] = nil
-	package.loaded["cmd"] = nil
-	package.loaded["lib.nix"] = nil
-	package.loaded["lib.nixpkgs_mapping"] = nil
+    package.loaded["file"] = nil
+    package.loaded["json"] = nil
+    package.loaded["cmd"] = nil
+    package.loaded["lib.nix"] = nil
+    package.loaded["lib.nixpkgs_mapping"] = nil
 end
 
 return M
